9491e8f78e719a81aec29a83a10a0c499b6a2e54
Kiril <fedechkin-kiril@mail.ru>
2022-11-09 09:29:17 +0300

    add adaptive for block

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,26 @@
 import React from "react";
 
+import emptyCart from "../assets/img/empty-cart.png";
+
 const NotFound: React.FC = () => {
-  return <div>NotFound</div>;
+  return (
+    <>
+      <div className="container container--cart">
+        <div className="cart cart--empty">
+          <h2>Корзина пустая 😕</h2>
+          <p>
+            Вероятней всего, вы не заказывали ещё пиццу.
+            <br />
+            Для того, чтобы заказать пиццу, перейди на главную страницу.
+          </p>
+          <img src={emptyCart} alt="Empty cart" />
+          <a href="/" className="button button--black">
+            <span>Вернуться назад</span>
+          </a>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default NotFound;
